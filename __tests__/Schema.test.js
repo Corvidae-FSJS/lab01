43c339cb5ca9bffe5e3e75c8203617441f53fd94
Dylan Corvidae <dylancorvidae@gmail.com>
2019-09-19 17:51:21 -0700

    added marty's demos

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -2,15 +2,41 @@ const Schema = require('../lib/Schema');
 
 describe('Schema', () => {
 
+
+//Marty's Demo
   // add a test schema
+  // const schema = new Schema({
+  //   name : {
+  //     type: String,
+  //     required: true
+  //   },
+  //   isFun: {
+  //     type:Boolean,
+  //     required:false
+  //   },
+  //   luckyNumber: {
+  //     type:Number,
+  //     required: false
+  //   }
+  // });
 
-  it('validates a correct model', () => {
 
-  });
 
-  it('throws on invalid model', () => {
+  // it('validates a correct model', () => {
+  //   const model = {
+  //     name: 'Good',
+  //     isFun: true,
+  //     luckyNumber: 79
+  //   };
+  //   const record = schema.validate(model);
 
-  });
+  //   expect(record).not.toBe(model);
+  //   expect(record).toEqual(model);
+  // });
+
+  // it('throws on invalid model', () => {
+
+  // });
 
   // more test cases...
 });
